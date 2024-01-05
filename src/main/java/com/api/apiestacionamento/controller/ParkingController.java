a00a399763a04b70fb6f8e9d9524ff3c18589433
Samuelsilvestre <fnndc90@gmail.com>
2024-01-05 14:28:03 -0300

    metodo post

--- src/main/java/com/api/apiestacionamento/controller/ParkingController.java
+++ src/main/java/com/api/apiestacionamento/controller/ParkingController.java
@@ -1,5 +1,38 @@
 package com.api.apiestacionamento.controller;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.apiestacionamento.dto.ParkingDto;
+import com.api.apiestacionamento.model.Parking;
+import com.api.apiestacionamento.service.ParkingService;
+
+import jakarta.validation.Valid;
+
+@RequestMapping("api/parking")
+@RestController
 public class ParkingController {
     
+    private ParkingService service;
+
+    public ParkingController(ParkingService service) {
+        this.service = service;
+
+    }
+
+    @PostMapping("/post")
+    public ResponseEntity<Object> save(@RequestBody @Valid ParkingDto dto) {
+        Parking model = new Parking();
+        BeanUtils.copyProperties(dto, model);
+        model.setDate(LocalDateTime.now(ZoneId.of("UTC")));
+        return ResponseEntity.ok().body(service.save(model));
+
+    }
 }
--- src/main/java/com/api/apiestacionamento/service/ParkingService.java
+++ src/main/java/com/api/apiestacionamento/service/ParkingService.java
@@ -1,5 +1,25 @@
 package com.api.apiestacionamento.service;
 
+import org.springframework.stereotype.Service;
+
+import com.api.apiestacionamento.model.Parking;
+import com.api.apiestacionamento.repository.ParkingRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
 public class ParkingService {
     
+    private ParkingRepository repository;
+
+    public ParkingService(ParkingRepository repository) {
+        this.repository = repository;
+
+    }
+
+    @Transactional
+    public Object save(Parking parking) {
+        return repository.save(parking);
+
+    }
 }
