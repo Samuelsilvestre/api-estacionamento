d638e6a53ddc4a42d30e2d89aeb79adcacdeb951
Samuelsilvestre <fnndc90@gmail.com>
2024-01-05 16:21:59 -0300

    metodos get

--- src/main/java/com/api/apiestacionamento/controller/ParkingController.java
+++ src/main/java/com/api/apiestacionamento/controller/ParkingController.java
@@ -1,11 +1,17 @@
 package com.api.apiestacionamento.controller;
 
+import java.math.BigInteger;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.List;
 import java.util.Optional;
+
+import javax.swing.text.html.Option;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,7 +39,7 @@ public class ParkingController {
 
         if (service.hasName(dto.getName())) {
             return ResponseEntity.badRequest().body("nome de estacionamento ja esta cadastrado.");
-            
+
         }
 
         Parking model = new Parking();
@@ -41,5 +47,25 @@ public class ParkingController {
         model.setDate(LocalDateTime.now(ZoneId.of("UTC")));
         return ResponseEntity.ok().body(service.save(model));
 
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<List<Parking>> listParking() {
+        return ResponseEntity.ok().body(service.getAll());
+
+
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Object> getOne(@PathVariable BigInteger id) {
+        Optional<Parking> optional = service.getOne(id);
+        
+        if (optional.isPresent()) {
+            return ResponseEntity.ok().body(optional.get());
+
+        }
+
+        return ResponseEntity.notFound().build();
+        
     }
 }
--- src/main/java/com/api/apiestacionamento/service/ParkingService.java
+++ src/main/java/com/api/apiestacionamento/service/ParkingService.java
@@ -1,7 +1,11 @@
 package com.api.apiestacionamento.service;
-
+import java.math.BigInteger;
+import java.util.List;
 import java.util.Optional;
 
+import javax.swing.text.html.Option;
+
+import org.hibernate.type.descriptor.jdbc.BigIntJdbcType;
 import org.springframework.stereotype.Service;
 
 import com.api.apiestacionamento.model.Parking;
@@ -24,9 +28,19 @@ public class ParkingService {
 
     }
 
+    public List<Parking> getAll() {
+        return repository.findAll();
+
+    }
+
     @Transactional
     public Object save(Parking parking) {
         return repository.save(parking);
 
+    }
+    
+    public Optional<Parking> getOne(BigInteger id) {
+        return repository.findById(id);
+        
     }
 }
